5a8cb5585292c94782426db096874c10bae3c448
Royalprm <95142405+Royalprm@users.noreply.github.com>
2021-12-01 22:17:37 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Упражнение_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\Обучение\Программирование Revit\Модуль 1 Видео");
+            Console.WriteLine("Список каталогов");
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("==Список подкаталогов==");
+                foreach (var it in item.GetDirectories())
+                    Console.WriteLine(it.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Список файлов");
+            foreach (var item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+}
+
+
